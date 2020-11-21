cda6e6bfae9b755c8735f7e261355a8de65aae03
Denny Mathew <devdenmathew@gmail.com>
2020-11-21 12:11:26 +0530

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Denny Mathew on 21/11/20.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
